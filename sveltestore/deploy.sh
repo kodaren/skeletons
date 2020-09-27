3c211e8dc22b2ca24e1918f5bb5f60e10cfb9cbf
Jimmy Karlsson <kodaren@gmail.com>
2020-09-27 22:19:24 +0200

    Added deploy script and updated to use label latest in sveltestore.yaml

---
+++ sveltestore/deploy.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/sh
+
+VERSION=latest
+docker build . -t kodaren/sveltestore:$VERSION
+docker push kodaren/sveltestore:$VERSION
+kubectl apply -f sveltestore.yaml
+kubectl rollout restart deploy -n kodaren sveltestore
