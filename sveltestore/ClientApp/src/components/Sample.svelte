41776be33a03fdca33bc32b45925450edf4d8788
Jimmy Karlsson <kodaren@gmail.com>
2020-09-27 21:57:36 +0200

    Added sample asp.net core sapper material ui project

---
+++ sveltestore/ClientApp/src/components/Sample.svelte
@@ -0,0 +1,34 @@
+<script>
+  import {Sample} from './Sample.ts';
+  import Button, {Label, Icon} from '@smui/button';
+  let clicked = new Sample().clicked;
+</script>
+
+<style>
+  .grayed {
+    opacity: .6;
+  }
+
+  .container {
+    display: flex;
+    flex-direction: column;
+  }
+
+  p {
+    text-align: center;
+  }
+</style>
+
+<div class="container">
+  <Button on:click={() => clicked++}>
+    <Icon class="material-icons">thumb_up</Icon>
+    <Label>Click Me</Label>
+  </Button>
+  <p>
+    {#if clicked}
+      You've clicked the material button {clicked} time{clicked === 1 ? '' : 's'}.
+    {:else}
+      <span class="grayed">You haven't clicked the material button.</span>
+    {/if}
+  </p>
+</div>
